59ccc5b89cfd12631399820ab83e6976154017cf
I an <ydh8485@gmail.com>
2021-04-16 07:32:23 +0900

    Counter components,containers,modules

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+import Counter from "../components/Counter";
+import { increase, decrease } from "../modules/counter";
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return <Counter onIncrease={increase} onDecrease={decrease} number={number} />;
+};
+
+const mapStateToProps = (state) => ({
+  number: state.counter,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease: () => {
+    dispatch(decrease());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
---
+++ src/modules/counter.js
@@ -0,0 +1,41 @@
+import { createAction, handleActions } from "redux-actions";
+
+// 액션 타입
+const INCREASE = "counter/INCREASE";
+const DECREASE = "coutner/DECREASE";
+
+// 액션 생성 함수
+// 1. 기본
+export const increase = () => ({
+  type: INCREASE,
+});
+
+// 2. createAction
+export const decrease = createAction(DECREASE);
+
+// 초기 상태
+const initialState = 0;
+
+// 리듀서
+// 1. 기본
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREASE:
+      return (state = state + 1);
+    case DECREASE:
+      return (state = state - 1);
+    default:
+      return state;
+  }
+};
+
+// handleActions
+// const counter = handleActions(
+//   {
+//     [INCREASE]: (state) => state + 1,
+//     [DECREASE]: (state) => state - 1,
+//   },
+//   initialState
+// );
+
+export default counter;
